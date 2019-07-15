a38cda910ddd061c28081c97e48e82456a87a21e
skin93 <arro.ciesielski@gmail.com>
2019-07-15 12:37:38 +0200

    UI added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { MdEdit, MdDelete } from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expense }) => {
+    const { id, charge, amount } = expense;
     return (
-        <div>
-            Hello from ExpenseItem
-        </div>
-    )
-}
-
+        <li className="item">
+            <div className="info">
+                <span className="expense">{charge}</span>
+                <span className="amount">${amount}</span>
+            </div>
+            <div>
+                <button className="edit-btn" arial-label="edit-button">
+                    <MdEdit />
+                </button>
+                <button className="clear-btn" arial-label="delete-button">
+                    <MdDelete />
+                </button>
+            </div>
+        </li>
+    );
+};
 export default ExpenseItem
